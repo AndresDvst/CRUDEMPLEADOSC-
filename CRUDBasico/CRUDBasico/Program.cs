2de34d0386dd210ae4dcbba7ea6f144b865b9ab4
Andres Campos <124460156+AndresDvst@users.noreply.github.com>
2025-03-15 16:33:16 -0400

    Agregacion Carpeta (#1)
    Agregacion carpeta y creacion de rama

---
+++ CRUDBasico/CRUDBasico/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CRUDBasico.Vistas;
+
+
+namespace CRUDBasico
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
